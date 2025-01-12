6e83829f466ec085184a7145e654857a971538b3
Claus Malter <3120297+cloonix@users.noreply.github.com>
2025-01-12 18:33:41 +0100

    game layer

--- keychron_k8_pro/keymap.c
+++ keychron_k8_pro/keymap.c
@@ -1,4 +1,4 @@
-﻿#include QMK_KEYBOARD_H
+#include QMK_KEYBOARD_H
 
 enum layers{
   MAC_BASE, // 0 - DIP switch down
@@ -83,6 +83,15 @@ _______,  _______,       _______,       _______,        _______,        _______,
 _______,  _______,       _______,       _______,        _______,        _______,      _______, _______,  _______,  _______,    _______,  _______,  _______,  _______,
 _______,  _______,       _______,       _______,        _______,        _______,      _______, _______,  _______,  _______,    _______,  _______,            _______,                 _______,
 _______,  _______,       _______,                                                     _______,                                 _______,  _______,  _______,  _______,  _______,       _______,  _______
+),
+
+[GAME] = LAYOUT_tkl_iso(
+KC_ESC,          KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             TD(PRINT),  KC_MUTE,  RGB_MOD,
+QK_LEAD,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,     KC_HOME,  KC_PGUP,
+KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_DEL,     KC_END,   KC_PGDN,
+KC_NO,           KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
+KC_LSFT,         KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,             KC_UP,
+KC_LCTL,         KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI, MO(WIN_FN),KC_RCTL,  KC_LEFT,   KC_DOWN,  KC_RGHT
 ),
 
 };
@@ -187,6 +196,11 @@ void leader_end_user(void) {
     did_leader_succeed = true;
   }
   
+  if (leader_sequence_one_key(KC_3)) {
+    layer_move(GAME);
+    did_leader_succeed = true;
+  }
+  
   if (!did_leader_succeed) {
     tap_code(KC_GRV);
   }
@@ -221,6 +235,9 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
               break;
             case 5:
               rgb_matrix_set_color(i, RGB_WHITE);
+              break;
+            case 6:
+              rgb_matrix_set_color(i, RGB_RED);
               break;
            }
 /*          
